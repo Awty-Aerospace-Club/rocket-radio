dffe8c229369aba7b25b12551199b3b2e58f6158
Tarek Ali <tarekali15@outlook.com>
2020-04-22 11:23:23 -0500

    informative comment

--- python_code/analysis.py
+++ python_code/analysis.py
@@ -1,3 +1,5 @@
+""" This file's purpose is to convert csv produced by receiver.py into database fields that can be addressed by a react-graphing app...
+soon to come"""
 import csvquery, mysql.connector, yaml
 
 data = csvquery.open_csv("output.csv")
