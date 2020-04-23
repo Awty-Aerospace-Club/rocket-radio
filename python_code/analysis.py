6960ea32ea8e5953651dc4f08e619cc1be9539da
Tarek Ali <tarekali15@outlook.com>
2020-04-23 09:27:02 -0500

    another bug fix

--- python_code/analysis.py
+++ python_code/analysis.py
@@ -1,6 +1,6 @@
 """ This file's purpose is to convert csv produced by receiver.py into database fields, which will be inserted into a MySQL db, that can be addressed by a react-graphing app...
 soon to come"""
-import csv, MySQLdb
+import csv, MySQLdb, yaml
 
 with open('database-cfg.yaml') as cfg:
     config = yaml.load(cfg, Loader=yaml.FullLoader)
