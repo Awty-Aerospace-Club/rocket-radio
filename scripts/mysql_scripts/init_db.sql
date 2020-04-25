0c53396b8d664c68f7146d364a06ab77224e09a1
Tarek Ali <tarekali15@outlook.com>
2020-04-25 10:30:26 -0500

    fixed dumb syntax err I made in init_db.sql

--- scripts/mysql_scripts/init_db.sql
+++ scripts/mysql_scripts/init_db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE RocketRadio;
 USE RocketRadio;
 
-CREATE TABLE SensorData (time double, altitude double, accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
+CREATE TABLE SensorData (time double, altitude double, accelX double, accelY double, accelZ double, gyroX double, gyroY double, gyroZdouble);
