9b96a8dd0cea124b8467d3652472945035f34ec5
Tarek Ali <tarekali15@outlook.com>
2020-04-25 10:37:30 -0500

    mysql wtfmoment

--- scripts/mysql_scripts/init_db.sql
+++ scripts/mysql_scripts/init_db.sql
@@ -1,4 +1,13 @@
 CREATE DATABASE RocketRadio;
 USE RocketRadio;
 
-CREATE TABLE SensorData (time, altitude, accelX, accelY, accelZ, gyroX, gyroY, gyroZ double);
+CREATE TABLE SensorData (
+	time,
+	altitude,
+	accelX,
+	accelY,
+	accelZ,
+	gyroX,
+	gyroY,
+	gyroZ double
+);
