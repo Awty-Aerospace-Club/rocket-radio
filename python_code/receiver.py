9ac39222cd0e4919fe2dcd38d68d43d742543e81
matt-mekha <mtmekha8@gmail.com>
2020-05-01 22:42:53 -0500

    attempted pipenv, did not work

--- python_code/receiver.py
+++ python_code/receiver.py
@@ -1,5 +1,7 @@
-# receiver.py's purpose is to unencode received serialized data from the arduino, then write it to csv
-import serial, csvquery, math
+# receiver.py's purpose is to decode serialized data from the Arduino, then write it to csv
+from csvquery import open_csv
+from math import degrees
+from serial import Serial, SerialException
 
 fields = [
     "time",
@@ -12,65 +14,72 @@ fields = [
     "gyroZ",
 ]
 
+
 def serial_ports():
     result = []
     for i in range(1, 257):
         port = f"COM{i}"
         try:
-            s = serial.Serial(port)
+            s = Serial(port)
             s.close()
             result.append(port)
-        except (OSError, serial.SerialException):
+        except (OSError, SerialException):
             pass
     return result
 
-print()
-ports = serial_ports()
-if len(ports) == 0:
-    print("No ports found")
-    exit(0)
-
-port = ports[0]
-if len(ports) > 1:
-    print("Multiple ports found, select one:")
-    print(", ".join(ports))
-    port = input(">> ")
-else:
-    print(f"Selected only available port: {port}")
-
-s = serial.Serial(port)
-file = open("output.csv", "w+", newline="")
 
 def validate(line):
     try:
         line = line.decode("ASCII")
         assert len(line) > 3 and line.count(",") == len(fields) and line[-2:] == "\r\n"
-        
+
         line = line[:-3]
         for num in line.split(","):
             float(num)
-        
+
         return line
-    except:
+    except (AssertionError, ValueError):
         print("[ERROR] received bad data")
         return False
-        
 
-print(",".join(fields), file=file)
-while True:
-    line = s.readline()
-    if not line: break
 
-    line = validate(line)
-    if not line: continue
+def main():
+    print()
+    ports = serial_ports()
+    if len(ports) == 0:
+        print("No ports found")
+        exit(-1)
 
-    print(line)
-    print(line, file=file)
+    port = ports[0]
+    if len(ports) > 1:
+        print("Multiple ports found, select one:")
+        print(", ".join(ports))
+        port = input(">> ")
+    else:
+        print(f"Selected only available port: {port}")
 
-file.close()
+    s = Serial(port)
+    file = open("output.csv", "w+", newline="")
 
-# once the code gets here, arduino is unplugged and the mission is over, time for data analysis
+    print(",".join(fields), file=file)
+    while True:
+        line = s.readline()
+        if not line:
+            break
 
-dataset = csvquery.open_csv("output.csv")
-dataset.replace(["gyroX", "gyroY", "gyroZ"], math.degrees)
-dataset.save_csv("output.csv")
+        line = validate(line)
+        if not line:
+            continue
+
+        print(line)
+        print(line, file=file)
+
+    file.close()
+
+    dataset = open_csv("output.csv")
+    dataset.replace(["gyroX", "gyroY", "gyroZ"], degrees)
+    dataset.save_csv("output.csv")
+
+
+if __name__ == "__main__":
+    main()
