da21727e86459cf08b6c9deb2b81af416e680290
Tarek Ali <tarekali15@outlook.com>
2020-05-11 06:51:34 -0500

    added multiline chart with line legend

--- python_code/chart_csv.py
+++ python_code/chart_csv.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from datetime import datetime
 import csv
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 headers = ['altitude', 'accelX', 'accelY', 'accelZ', 'accelZ', 'gyroX', 'gyroY', 'gyroZ']
 df = pd.read_csv("output.csv", names=headers)
 print(df)
 
 for i in headers:
-    plt.plot(df['time'], df[i])
+    plt.plot(df['time'], df[i], label=i)
+
+plt.legend(loc="upper left")
+plt.show()
