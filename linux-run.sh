ce05caaf1dacb8e61722d8dac375b8b8ff7e7fe8
Tarek Ali <tarekali15@outlook.com>
2020-07-13 19:06:52 -0500

    ran one command as root as to skirt doing tons of BS configuratoin

--- linux-run.sh
+++ linux-run.sh
@@ -1,5 +1,5 @@
 sudo apt install python3.8
 pip3 install pipenv
 ./scripts/pipenv-automation.sh
-python3 python_code/receiver.py
+sudo python3 python_code/receiver.py
 python3 python_code/chart_csv.py
